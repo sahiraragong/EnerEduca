cc20f9bbf065c97212f68ebd01303a4548c0001b
Sahir Aragon <sahiraragongucci@gmail.com>
2025-10-26 16:39:12 -0500

    actualiza workflow

--- energeduca/wsgi.py
+++ energeduca/wsgi.py
@@ -1,17 +1,9 @@
-"""
-WSGI config for energeduca project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise  # importar WhiteNoise
+from whitenoise import WhiteNoise
+from django.conf import settings  # ← importante para acceder a STATIC_ROOT
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energeduca.settings')
 
 application = get_wsgi_application()
-# Aquí se envuelve la aplicación con WhiteNoise para servir archivos estáticos
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
